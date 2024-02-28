f782890c1b3eca47fb618365bc7d1e7e835e74d9
Preet Suthar <preetsutharxd@gmail.com>
2024-02-28 21:24:20 +0530

    Update CodeEditor component and styles

--- src/components/CodeEditor.js
+++ src/components/CodeEditor.js
@@ -1,16 +1,19 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 export default function CodeEditor({ onCodeSubmit }) {
-  const [code, setCode] = useState('');
+  const [code, setCode] = useState("");
 
   return (
-    <div>
+    <div className="code-editor-div">
       <textarea
+        className="code-editor-textarea"
         value={code}
         onChange={(e) => setCode(e.target.value)}
         placeholder="Type your code here..."
       />
-      <button onClick={() => onCodeSubmit(code)}>Snippixify code</button>
+      <button className="primary-button" onClick={() => onCodeSubmit(code)}>
+        Snippixify code
+      </button>
     </div>
   );
 }

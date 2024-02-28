987095efcd943ed23fc7d96873c0e689ab4d5064
Preet Suthar <preetsutharxd@gmail.com>
2024-02-28 21:24:32 +0530

    Add buttons.scss to partials

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,6 +2,7 @@ import "@/styles/globals.scss";
 import "@/styles/partials/_showcase.scss";
 import "@/styles/partials/_footer.scss";
 import "@/styles/partials/_navbar.scss";
+import "@/styles/partials/_buttons.scss";
 import "@/styles/partials/_code_editor.scss";
 import "@/styles/partials/_code_image.scss";
 

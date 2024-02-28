0c69fe7a9d66c0d6afd116738d607f46645a7d50
Preet Suthar <preetsutharxd@gmail.com>
2024-02-28 21:24:27 +0530

    Refactor CodeImage component to dynamically adjust image size for download

--- src/components/CodeImage.js
+++ src/components/CodeImage.js
@@ -5,11 +5,18 @@ import { useRef } from "react";
 
 export default function CodeImage({ code }) {
   const ref = useRef();
-
   const downloadImage = async () => {
-    if (ref.current === null) {
+    if (!ref.current) {
       return;
     }
+
+    // Save the original size
+    const originalWidth = ref.current.style.width;
+    const originalHeight = ref.current.style.height;
+
+    // Adjust the size to fit the content
+    ref.current.style.width = "max-content";
+    ref.current.style.height = "max-content";
 
     toPng(ref.current, { cacheBust: true })
       .then((dataUrl) => {
@@ -17,6 +24,10 @@ export default function CodeImage({ code }) {
         link.download = "code-image.png";
         link.href = dataUrl;
         link.click();
+
+        // Restore the original size
+        ref.current.style.width = originalWidth;
+        ref.current.style.height = originalHeight;
       })
       .catch((err) => {
         console.error("oops, something went wrong!", err);
@@ -24,18 +35,19 @@ export default function CodeImage({ code }) {
   };
 
   return (
-    <>
-      <div style={{ backgroundColor: "", padding: "20px" }}>
+    <div className="code-image-div">
+      <div ref={ref}>
         <SyntaxHighlighter
           language="javascript"
           style={vscDarkPlus}
           showLineNumbers={true}
-          wrapLongLines={true}
         >
           {code}
         </SyntaxHighlighter>
       </div>
-      <button onClick={downloadImage}>Download Image</button>
-    </>
+      <button onClick={downloadImage} className="primary-button">
+        Download Image
+      </button>
+    </div>
   );
 }

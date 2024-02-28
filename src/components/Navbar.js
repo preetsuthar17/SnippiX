a3b34532e61414b9a4296fa06f7010fe9c44416a
Preet Suthar <preetsutharxd@gmail.com>
2024-02-28 20:40:33 +0530

    Add navbar component with logo and links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,38 @@
+import Link from "next/link";
+import Image from "next/image";
+
+import snippix_logo from "../../public/snippix-no-bg-logo.svg";
+
 export const Navbar = () => {
   return (
     <>
-      <nav></nav>
+      <nav className="navbar">
+        <div className="navbar-header">
+          <Link href="/">
+            <Image
+              src={snippix_logo}
+              width={60}
+              height={60}
+              priority={true}
+              alt="SnippiX"
+            />
+          </Link>
+        </div>
+        <div className="navbar-links">
+          <ul>
+            <li>
+              <Link href="https://github.com/preetsuthar17" target="_blank">
+                Github
+              </Link>
+            </li>
+            <li>
+              <Link href="https://preetsuthar.me" target="_blank">
+                Owner
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
     </>
   );
 };

de8a101355eedf5e762e58bf355efefc01ab49d6
Preet Suthar <preetsutharxd@gmail.com>
2024-02-28 21:28:43 +0530

    Add title to Head component in index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,15 @@
 import { Layout } from "@/components/Layout";
 import { Showcase } from "@/components/Showcase";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <Layout>
+      <Head>
+        <title>
+          SnippiX - Easily convert your code snippets in beautiful images.
+        </title>
+      </Head>
       <div>
         <Showcase />
       </div>

acf3eb3b603c437b6c2f8cf5db5c7902586f5aa3
Preet Suthar <preetsutharxd@gmail.com>
2024-02-29 05:44:38 +0000

    Add functionality to select theme for the code snippet

--- src/components/CodeImage.js
+++ src/components/CodeImage.js
@@ -1,16 +1,66 @@
+import { useState, useRef } from "react";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { vscDarkPlus } from "react-syntax-highlighter/dist/cjs/styles/prism";
+import { a11yDark, atomDark, base16AteliersulphurpoolLight, cb, coldarkCold, coldarkDark, coy, dracula, dark, duotoneDark, duotoneEarth, duotoneForest, duotoneLight, duotoneSea, duotoneSpace, funky, ghcolors, hopscotch, materialDark, materialLight, materialOceanic, nightOwl, nord, okaidia, oneDark, oneLight, pojoaque, prism, shadesOfPurple, solarizedlight, synthwave84, tomorrow, twilight, vs, vsDark, vscDarkPlus, xonokai } from "react-syntax-highlighter/dist/cjs/styles/prism";
 import { toPng } from "html-to-image";
-import { useRef, useState } from "react";
+
+const themes = {
+  vscDarkPlus,
+  a11yDark,
+  atomDark,
+  base16AteliersulphurpoolLight,
+  cb,
+  coldarkCold,
+  coldarkDark,
+  coy,
+  dracula,
+  dark,
+  duotoneDark,
+  duotoneEarth,
+  duotoneForest,
+  duotoneLight,
+  duotoneSea,
+  duotoneSpace,
+  funky,
+  ghcolors,
+  hopscotch,
+  materialDark,
+  materialLight,
+  materialOceanic,
+  nightOwl,
+  nord,
+  okaidia,
+  oneDark,
+  oneLight,
+  pojoaque,
+  prism,
+  shadesOfPurple,
+  solarizedlight,
+  synthwave84,
+  tomorrow,
+  twilight,
+  vs,
+  vsDark,
+  xonokai
+};
 
 export default function CodeImage({ code }) {
-  const ref = useRef();
+  const [selectedTheme, setSelectedTheme] = useState(vscDarkPlus);
   const [fontSize, setFontSize] = useState(16);
+  const ref = useRef();
 
+  const handleThemeChange = (event) => {
+    const themeName = event.target.value;
+    setSelectedTheme(themes[themeName]);
+  };
+
+  const handleFontSizeChange = (event) => {
+    const newFontSize = parseInt(event.target.value);
+    setFontSize(newFontSize);
+    document.documentElement.style.setProperty("--font-size", `${newFontSize}px`);
+  };
   const downloadImage = async () => {
-    if (!ref.current) {
-      return;
-    }
+    if (!ref.current) { return; }
+
 
     const originalWidth = ref.current.style.width;
     const originalHeight = ref.current.style.height;
@@ -37,34 +87,27 @@ export default function CodeImage({ code }) {
         });
     }, 0);
   };
-
-  const handleFontSizeChange = (event) => {
-    const newFontSize = parseInt(event.target.value);
-    setFontSize(newFontSize);
-    document.documentElement.style.setProperty(
-      "--font-size",
-      `${newFontSize}px`
-    );
-  };
-
   return (
     <div className="code-image-div">
-      <div class="PB-range-slider-div">
-        <small>Font Size</small>
-        <input type="range" id="myRange"
-          min="13"
-          max="40"
-          value={fontSize}
-          onChange={handleFontSizeChange}
-          className="font-size-slider PB-range-slider" />
-
+      <div className="code-image-config-options">
+        <div className="theme-select-div">
+          <select onChange={handleThemeChange} className="theme-selector">
+            {Object.keys(themes).map((theme, index) => (
+              <option key={index} value={theme}>{theme}</option>
+            ))}
+          </select>
+        </div>
+        <div className="PB-range-slider-div"> <small>Font Size</small> <input type="range" id="myRange" min="13" max="40" value={fontSize} onChange={handleFontSizeChange} className="font-size-slider PB-range-slider" />
+        </div>
       </div>
+
       <div ref={ref}>
         <SyntaxHighlighter
           className="code-block"
           language="javascript"
-          style={vscDarkPlus}
+          style={selectedTheme}
           showLineNumbers={true}
+
         >
           {code}
         </SyntaxHighlighter>

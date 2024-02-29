b4c00ad4d298fd1915754467323300fcafb05f11
Preet Suthar <preetsutharxd@gmail.com>
2024-02-29 07:14:08 +0530

    Update CodeImage component to include font size slider

--- src/components/CodeImage.js
+++ src/components/CodeImage.js
@@ -1,43 +1,62 @@
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { vscDarkPlus } from "react-syntax-highlighter/dist/cjs/styles/prism";
 import { toPng } from "html-to-image";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 export default function CodeImage({ code }) {
   const ref = useRef();
+  const [fontSize, setFontSize] = useState(16);
+
   const downloadImage = async () => {
     if (!ref.current) {
       return;
     }
 
-    // Save the original size
     const originalWidth = ref.current.style.width;
     const originalHeight = ref.current.style.height;
 
-    // Adjust the size to fit the content
     ref.current.style.width = "max-content";
     ref.current.style.height = "max-content";
 
-    toPng(ref.current, { cacheBust: true })
-      .then((dataUrl) => {
-        const link = document.createElement("a");
-        link.download = "code-image.png";
-        link.href = dataUrl;
-        link.click();
+    setTimeout(() => {
+      toPng(ref.current, { cacheBust: true })
+        .then((dataUrl) => {
+          const link = document.createElement("a");
+          link.download = "code-image.png";
+          link.href = dataUrl;
+          link.click();
 
-        // Restore the original size
-        ref.current.style.width = originalWidth;
-        ref.current.style.height = originalHeight;
-      })
-      .catch((err) => {
-        console.error("oops, something went wrong!", err);
-      });
+          ref.current.style.width = originalWidth;
+          ref.current.style.height = originalHeight;
+        })
+        .catch((err) => {
+          console.error("oops, something went wrong!", err);
+        });
+    }, 0);
+  };
+
+  const handleFontSizeChange = (event) => {
+    const newFontSize = parseInt(event.target.value);
+    setFontSize(newFontSize);
+    document.documentElement.style.setProperty(
+      "--font-size",
+      `${newFontSize}px`
+    );
   };
 
   return (
     <div className="code-image-div">
+      <input
+        type="range"
+        min="13"
+        max="40"
+        value={fontSize}
+        onChange={handleFontSizeChange}
+        className="font-size-slider"
+      />
       <div ref={ref}>
         <SyntaxHighlighter
+          className="code-block"
           language="javascript"
           style={vscDarkPlus}
           showLineNumbers={true}

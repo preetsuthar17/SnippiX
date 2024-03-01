1b2b95019a3450cb5f47bd1ab5838ec3726d89a2
Preet Suthar <preetsutharxd@gmail.com>
2024-03-01 09:50:06 +0000

    Added watermark and functionality to toggle watermark

--- src/components/CodeImage.js
+++ src/components/CodeImage.js
@@ -280,6 +280,7 @@ export default function CodeImage({ code }) {
   const [fontSize, setFontSize] = useState(16);
   const [margin, setMargin] = useState(0);
   const [boxShadow, setBoxShadow] = useState("");
+  const [watermark, setWatermark] = useState(true);
   const [selectedLanguage, setSelectedLanguage] = useState("javascript");
   const [color, setColor] = useState('#fff');
   const [settingsDropdown, setSettingsDropdown] = useState(false)
@@ -382,6 +383,9 @@ export default function CodeImage({ code }) {
     setBoxShadow(prevState => !prevState);
     const newBoxShadow = boxShadow ? "" : "0 20px 68px rgba(0, 0, 0, 0.55)";
     document.documentElement.style.setProperty("--box-shadow", newBoxShadow);
+  };
+  const handleWatermarkToggle = () => {
+    setWatermark(!watermark)
   };
 
 
@@ -487,9 +491,12 @@ export default function CodeImage({ code }) {
             <div className="settings-dropdown-toggle" onClick={handleSettingsDropdown}>
               <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="currentColor" d="M10.825 22q-.675 0-1.162-.45t-.588-1.1L8.85 18.8q-.325-.125-.612-.3t-.563-.375l-1.55.65q-.625.275-1.25.05t-.975-.8l-1.175-2.05q-.35-.575-.2-1.225t.675-1.075l1.325-1Q4.5 12.5 4.5 12.337v-.675q0-.162.025-.337l-1.325-1Q2.675 9.9 2.525 9.25t.2-1.225L3.9 5.975q.35-.575.975-.8t1.25.05l1.55.65q.275-.2.575-.375t.6-.3l.225-1.65q.1-.65.588-1.1T10.825 2h2.35q.675 0 1.163.45t.587 1.1l.225 1.65q.325.125.613.3t.562.375l1.55-.65q.625-.275 1.25-.05t.975.8l1.175 2.05q.35.575.2 1.225t-.675 1.075l-1.325 1q.025.175.025.338v.674q0 .163-.05.338l1.325 1q.525.425.675 1.075t-.2 1.225l-1.2 2.05q-.35.575-.975.8t-1.25-.05l-1.5-.65q-.275.2-.575.375t-.6.3l-.225 1.65q-.1.65-.587 1.1t-1.163.45zm1.225-6.5q1.45 0 2.475-1.025T15.55 12q0-1.45-1.025-2.475T12.05 8.5q-1.475 0-2.488 1.025T8.55 12q0 1.45 1.013 2.475T12.05 15.5" /></svg></div>
             {settingsDropdown && (
-              <div className="settings-dropdown-menu">
+              <div className="settings-dropdown-menu-content">
                 <label>
-                  <input type="checkbox" value={boxShadow} onClick={handleBoxShadowToggle} /> box shadow
+                  <input type="checkbox" onClick={handleBoxShadowToggle} /> box shadow
+                </label>
+                <label>
+                  <input type="checkbox" checked={watermark} onClick={handleWatermarkToggle} /> Watermark
                 </label>
               </div>
             )
@@ -506,6 +513,11 @@ export default function CodeImage({ code }) {
         >
           {code}
         </SyntaxHighlighter>
+        <div className="watermark-content">
+          {watermark && (
+            <p>Made with SnippiX</p>
+          )}
+        </div>
       </div>
       <button onClick={downloadImage} className="primary-button">
         Download Image

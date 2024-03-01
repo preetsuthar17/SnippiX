b8a76a452287a070a5c58d0cdcce4178f28c588d
Preet Suthar <preetsutharxd@gmail.com>
2024-03-01 07:29:40 +0000

    Add intial demo code whenever user opens the website

--- src/components/CodeEditor.js
+++ src/components/CodeEditor.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 export default function CodeEditor({ onCodeSubmit }) {
-  const [code, setCode] = useState("");
+  const [code, setCode] = useState("function resolveAfter2Seconds() {\n    return new Promise((resolve) => { \n      setTimeout(() => { \n        resolve('resolved');\n      }, 2000);\n    });\n  }\n\n async function asyncCall() {\n    console.log('calling'); \n    const result = await resolveAfter2Seconds();\n    console.log(result);\n  }\n\nasyncCall();");
 
   return (
     <div className="code-editor-div">
--- src/components/Showcase.js
+++ src/components/Showcase.js
@@ -3,7 +3,7 @@ import CodeImage from "./CodeImage";
 import { useState } from "react";
 
 export const Showcase = () => {
-  const [code, setCode] = useState("");
+  const [code, setCode] = useState("function resolveAfter2Seconds() {\n    return new Promise((resolve) => { \n      setTimeout(() => { \n        resolve('resolved');\n      }, 2000);\n    });\n  }\n\n async function asyncCall() {\n    console.log('calling'); \n    const result = await resolveAfter2Seconds();\n    console.log(result);\n  }\n\nasyncCall();");
 
   return (
     <>

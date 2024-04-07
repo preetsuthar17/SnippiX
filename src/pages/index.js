95e803cd4e26981a865a96aa4466b458223f5215
Preet Suthar <preetsutharxd@gmail.com>
2024-04-07 17:08:41 +0530

    Update meta tags in index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,23 +6,13 @@ export default function Home() {
   return (
     <Layout>
       <Head>
-        <title>
-          SnippiX - Easily convert your code snippets in beautiful images.
-        </title>
+        <title>Easily convert your code snippets in beautiful images.</title>
         <meta name="robots" content="all" />
-        <meta
-          name="description"
-          content=" Easily convert your code snippets in beautiful images."
-        />
         <meta name="theme-color" content="#080a16" />
         <meta httpEquiv="content-language" content="en" />
         <meta httpEquiv="content-type" content="text/html; charset=UTF-8" />
         <meta
           property="og:title"
-          content=" SnippiX - Easily convert your code snippets in beautiful images."
-        />
-        <meta
-          property="og:description"
           content=" Easily convert your code snippets in beautiful images."
         />
         <meta property="og:url" content="https://snippix.netlify.app" />
@@ -33,18 +23,14 @@ export default function Home() {
         />
         <meta
           name="keywords"
-          content="Preet Suthar, carbon.now.sh, snappify, code snippets, code image snippet, code to image converter, code image, code image generator, code to image, code snippet generator, code snippet image generator"
+          content="Preet Suthar, carbon.now.sh, snappify, code snippets, code image snippet, code to image converter, code image, code image generator, code to image, code snippet generator, code snippet image generator , ray.so, ray so"
         />
         <meta name="author" content="Preet Suthar" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta
           name="twitter:title"
-          content=" SnippiX - Easily convert your code snippets in beautiful images."
-        />
-        <meta
-          name="twitter:description"
           content=" Easily convert your code snippets in beautiful images."
-        />{" "}
+        />
         <meta name="subject" content="coding" />
       </Head>
       <div>

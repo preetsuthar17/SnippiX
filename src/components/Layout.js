8a0f4d99621d4ac33b6c560ae69ef327eb51dadc
Preet Suthar <preetsutharxd@gmail.com>
2024-04-07 17:07:51 +0530

    Remove Navbar component from Layout.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,10 +1,8 @@
 import { Footer } from "./Footer";
-import { Navbar } from "./Navbar";
 
 export const Layout = ({ children }) => {
   return (
     <>
-      <Navbar />
       <main>{children}</main>
       <Footer />
     </>

9f69234228f5f35fd3f8e6778830203a3823aa11
Preet Suthar <preetsutharxd@gmail.com>
2024-04-07 17:09:04 +0530

    Remove Navbar component and related styles

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,6 @@
 import "@/styles/globals.scss";
 import "@/styles/partials/_showcase.scss";
 import "@/styles/partials/_footer.scss";
-import "@/styles/partials/_navbar.scss";
 import "@/styles/partials/_buttons.scss";
 import "@/styles/partials/_code_editor.scss";
 import "@/styles/partials/_code_image.scss";
